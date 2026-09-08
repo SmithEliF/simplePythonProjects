04de5555c0e60099ce6601d481778eb2851a9aa4
smithEliF <smith.eli.frank@gmail.com>
2026-09-08 09:50:55 -0400

    finished game

--- higherLower/higherLower.py
+++ higherLower/higherLower.py
@@ -10,16 +10,28 @@ CHOICES = ['A', 'B']
 gameloop = True
 
 def startGame():
-    person1 = data[random.randint(0, len(data) - 1)]
-    person2 = data[random.randint(0, len(data) - 1)]
+    firstTime = True
+    score = 0
     while gameloop:
         os.system('clear')
         print(logo)
+        person1 = data[random.randint(0, len(data) - 1)]
+        person2 = data[random.randint(0, len(data) - 1)]
+        if not firstTime:
+            print(f"You're right! Current score: {score}.")
         print(f"Compare A: {person1['name']}, a {person1['description']}, from {person1['country']}.")
         print(vs)
         print(f"Compare B: {person2['name']}, a {person2['description']}, from {person2['country']}.")
         choice = input("Who has more followers? Type 'A' or 'B': ").upper()
         if choice in CHOICES:
-            print("good job")
-            break
+            if (person1['follower_count'] > person2['follower_count'] and choice == 'A') or (person1['follower_count'] < person2['follower_count'] and choice == 'B'):
+                print("good job")
+                score += 1
+                firstTime = False
+            else:
+                print("Sorry, that's wrong. Final score: " + str(score))
+                break
+        else:
+            print("Choose A or B only")
+            continue
 startGame()
