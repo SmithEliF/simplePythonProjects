eec386fa4b60392b39b4b9bf4951438a38290670
smithEliF <smith.eli.frank@gmail.com>
2026-09-09 16:04:58 -0400

    initial commit

--- oop-test/oop_test.py
+++ oop-test/oop_test.py
@@ -1,8 +1,30 @@
 class User:
-    pass
 
-user_1 = User()
-user_1.id = '001'
-user_1.username = 'Eli'
+    def __init__(self, user_id, username):
+#Self is the actual object being initialized
 
-print(user_1.username)
+# Initializing Attributes
+        self.id = user_id
+        self.username = username
+
+# Set attribute with default value
+        self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
+
+user_1 = User("001", "phil_pleeb")
+user_2 = User("002", "eli_smith")
+
+user_1.follow(user_2)
+
+print(user_1.following)
+print(user_1.followers)
+print(user_2.following)
+print(user_2.followers)
+
+# print(f"User {user_1.id}'s username is {user_1.username}")
+
+
